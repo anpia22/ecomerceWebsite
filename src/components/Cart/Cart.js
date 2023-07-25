a8357ae5f6ea2ea87efdec763537a61470e015b6
Anup Ashtankar <122741245+anpia22@users.noreply.github.com>
2023-07-25 22:23:34 +0530

    Create Cart.js

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,10 @@
+import './Cart.css'
+const Cart = (props) => {
+    return ( 
+        <div className="cart">
+            <h4>Cart <span>5</span></h4>
+        </div>
+     );
+}
+ 
+export default Cart;
