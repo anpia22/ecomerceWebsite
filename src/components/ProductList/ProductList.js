3ade66d11e26c5fdd30f446f43b44c4e7d32ec24
Anup Ashtankar <122741245+anpia22@users.noreply.github.com>
2023-07-25 22:59:41 +0530

    Update ProductList.js

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,8 +1,9 @@
+import './ProductList.css'
 const ProductList = (props) => {
     let allProduct = <ul>
         {props.productList.map((items) => {
             return (<li key={items.id}>
-                <span>{items.name}</span> <span>{items.description}</span> <span>{items.price}</span> <button>L {items.L}</button> <button>M {items.M}</button> <button>S {items.S}</button>
+                <span>{items.name}</span> <span>{items.description}</span> <span>{items.price}</span> <button>L: {items.L}</button> <button>M: {items.M}</button> <button>S: {items.S}</button>
             </li>)
         })}
     </ul>
