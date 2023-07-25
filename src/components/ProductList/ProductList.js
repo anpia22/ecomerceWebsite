8967ca5ed224aad9d5be2035012d6b58fb6c85bf
Anup Ashtankar <122741245+anpia22@users.noreply.github.com>
2023-07-25 22:33:37 +0530

    ProductList.js

---
+++ src/components/ProductList/ProductList.js
@@ -0,0 +1,16 @@
+const ProductList = (props) => {
+    let allProduct = <ul>
+        {props.productList.map((items) => {
+            return (<li key={items.id}>
+                <span>{items.name}</span> <span>{items.description}</span> <span>{items.price}</span> <button>L {items.L}</button> <button>M {items.M}</button> <button>S {items.S}</button>
+            </li>)
+        })}
+    </ul>
+    return (
+        <div>
+            {allProduct}
+        </div>
+    );
+}
+
+export default ProductList;
